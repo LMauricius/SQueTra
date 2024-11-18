c06b86e4907797b1c5ec3980cbb86510cc2a7ed4
LMauricius <mauriciosmit100@gmail.com>
2024-11-18 23:56:49 +0100

    Sized typedefs

---
+++ include/squetra/util/typedef.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef H_INCLUDED_SQUETRA_UTIL_TYPEDEF
+#define H_INCLUDED_SQUETRA_UTIL_TYPEDEF
+
+#include <cstddef>
+#include <cstdint>
+
+namespace squetra {
+
+using IndexT = std::uint32_t;
+using CountT = std::uint32_t;
+} // namespace squetra
+
+#endif // H_INCLUDED_SQUETRA_UTIL_TYPEDEF
